654740a45ad7f02b03b3a53df91d94b7dc8920b6
RoronoaTheuzo <canterimatheu@gmail.com>
2022-07-19 18:07:35 -0300

    Melhorias na Pokedex + Gif

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 const pokemonName = document.querySelector('.pokemon-name');
 const pokemonNumber = document.querySelector('.pokemon-number');
-const pokemonImage= document.querySelector('.pokemon-image');
-const formulario= document.querySelector('.formulario');
+const pokemonImage = document.querySelector('.pokemon-image');
+
+const formulario = document.querySelector('.formulario');
+
 const input = document.querySelector('.input-search');
-const buttonPrev = document.querySelector('.btn-prev');
-const buttonProx = document.querySelector('.btn-next');
-let searchPokemon = 1;
+const botaoAnt = document.querySelector('.btn-ant');
+const botaoProx = document.querySelector('.btn-prox');
+
+let procurarPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    if (APIResponse.status == 200){
+    if (APIResponse.status == 200) {
         const data = await APIResponse.json();
         return data;
     }
@@ -18,18 +21,18 @@ const fetchPokemon = async (pokemon) => {
 
 const renderPokemon = async (pokemon) => {
 
-    pokemonName.innerHTML = 'Loading...';
+    pokemonName.innerHTML = 'Carregando...';
     pokemonNumber.innerHTML = ' ';
 
     const data = await fetchPokemon(pokemon);
 
-    if (data){
+    if (data) {
         pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
         pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
         input.value = '';
-        searchPokemon = data.id;
+        procurarPokemon = data.id;
     } else {
         pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Não encontrado!';
@@ -42,18 +45,18 @@ formulario.addEventListener('submit', (event) => {
     renderPokemon(input.value);
 });
 
-buttonPrev.addEventListener('click', () => {
-    if (searchPokemon > 1){
-        searchPokemon -= 1;
-        renderPokemon(searchPokemon);
+botaoAnt.addEventListener('click', () => {
+    if (procurarPokemon > 1) {
+        procurarPokemon -= 1;
+        renderPokemon(procurarPokemon);
     }
 });
 
-buttonProx.addEventListener('click', () => {
-    searchPokemon += 1;
-    renderPokemon(searchPokemon);
+botaoProx.addEventListener('click', () => {
+    procurarPokemon += 1;
+    renderPokemon(procurarPokemon);
 });
 
-renderPokemon(searchPokemon);
+renderPokemon(procurarPokemon);
 
 
